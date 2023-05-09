b8d7ad4638738dcad3379911ed270b52bcc25e06
heshegto <heshkin@gmail.com>
2023-05-09 17:09:49 +0300

    lesson 4.12. Customised templates and form for user's personal account page

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.contrib import auth
 from users.models import User
-from users.forms import UserLoginForm, UserRegistrationForm
+from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 from django.urls import reverse
 
 
@@ -31,3 +31,21 @@ def register(request):
         form = UserRegistrationForm()
     context = {'form': form}
     return render(request, 'users/register.html', context)
+
+
+def profile(request):
+    if request.method == 'POST':
+        print('pidor')
+        form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
+        if form.is_valid():
+            print('chmo')
+            form.save()
+            return HttpResponseRedirect(reverse("users:profile"))
+        else:
+            print('gay')
+            print(form.errors)
+    else:
+        print('govno')
+        form = UserProfileForm(instance=request.user)
+    context = {'title': 'Store - Профиль', 'form': form}
+    return render(request, 'users/profile.html', context)
