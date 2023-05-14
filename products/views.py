8517f311871af11d67e6a563ddf779137ff494dd
heshegto <heshkin@gmail.com>
2023-05-14 14:26:21 +0300

    lesson 6.2. added category filtration

--- products/views.py
+++ products/views.py
@@ -12,13 +12,19 @@ def index(request):
     return render(request, 'products/index.html', content)
 
 
-def products(request):
+def products(request, category_id=None):
+    # if category_id:
+    #     category = ProductCategory.objects.get(id=category_id)
+    #     products = Product.objects.filter(category=category)
+    # else:
+    #     products = Product.objects.all()
     content = {
         'title': 'Store - Каталог',
-        'products': Product.objects.all(),
         'categories': ProductCategory.objects.all(),
+        'products': Product.objects.filter(category=category_id) if category_id else Product.objects.all()
     }
     return render(request, "products/products.html", content)
+
 
 @login_required
 def basket_add(request, product_id):
@@ -31,6 +37,7 @@ def basket_add(request, product_id):
         basket.quantity += 1
         basket.save()
     return HttpResponseRedirect(request.META['HTTP_REFERER'])
+
 
 @login_required
 def basket_remove(request, basket_id):
